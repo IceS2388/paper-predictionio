2cd343dc1199dcb2b35768d1ee531aa0c05a26ea
ices <2388627880@qq.com>
2019-06-30 21:02:36 +0800

    修改Pearson系数为1.5倍提交。

--- src/main/scala/org/example/recommendation/Serving.scala
+++ src/main/scala/org/example/recommendation/Serving.scala
@@ -18,54 +18,49 @@ class Serving
 
   /**
     * 本方法处理预测的结果。可聚合多个预测模型的预测结果。这里返回的是最终的结果。PredictionIO会自动转换成JSON格式。
+    * 思路：
+    *   不同算法预测出来的评分肯定不统一，为了方便计算必须归一化。再进行具体的权重系数必须仔细考虑。
     * */
   override
   def serve(query: Query,
     predictedResults: Seq[PredictedResult]): PredictedResult = {
-    //predictedResults.head //als
-    //predictedResults.size //方法的总数
+
+    //存储最终结果的HashMap
+    val result=new mutable.HashMap[String,Double]()
+
+
     logger.info(s"总的算法引擎数量：${predictedResults.size}")
 
-    logger.info("1.---原始评分---")
     //1.展示als的评分结果
     logger.info("ALS算法")
+    val alsResult=predictedResults.head
+
     var alsSum=0D
-    predictedResults.head.itemScores.foreach(r=>{
+    alsResult.itemScores.foreach(r=>{
       alsSum+=r.score
-      logger.info(r)
     })
+    //归一化后存储
+    alsResult.itemScores.foreach(r=>{
+     result.put(r.item,r.score/alsSum)
+    })
+
     logger.info("Pearson算法")
+    val pearsonResult=predictedResults.take(2).last
     var pearsonSum=0D
-    predictedResults.take(2).last.itemScores.foreach(r=>{
+    pearsonResult.itemScores.foreach(r=>{
       pearsonSum+=r.score
-      logger.info(r)
+
     })
-
-
-
-    /**
-      * 思路：
-      *   不同算法预测出来的评分肯定不统一，为了方便计算必须归一化。再进行具体的权重系数必须仔细考虑。
-      * */
-    val result=new mutable.HashMap[String,Double]()
-    predictedResults.map(pr=>{
-
-      //对不同模型的预测评分，进行归一化。
-      var sum=0D;
-      pr.itemScores.map(r=>{
-        sum+= r.score
-      })
-
-      pr.itemScores.map(r=>{
-        if(!result.contains(r.item)){
-          result.put(r.item,r.score/sum)
-        }else{
-          //其他算法提供的预测结果
-          val oldScore:Double =result.get(r.item).get
-          result.put(r.item,r.score/sum+oldScore)
-        }
-      })
-
+    //归一化后存储
+    pearsonResult.itemScores.foreach(r=>{
+      //这里设置pearson的系数权重
+      if(!result.contains(r.item)){
+        result.put(r.item,r.score/pearsonSum)
+      }else{
+        //其他算法提供的预测结果
+        val oldScore:Double =result.get(r.item).get
+        result.put(r.item,1.5*r.score/pearsonSum+oldScore)
+      }
     })
 
     logger.info("2.---归一化评分---")
@@ -77,7 +72,7 @@ class Serving
     })
     logger.info("Pearson算法")
     predictedResults.take(2).last.itemScores.foreach(r=>{
-      logger.info(s"item:${r.item},score:${r.score/pearsonSum}")
+      logger.info(s"item:${r.item},1.5*score:${1.5*r.score/pearsonSum}")
     })
 
     PredictedResult(result.map(r=>new ItemScore(r._1,r._2)).toArray.sortBy(_.score).reverse.take(query.num))
