b19cf8a5ea5809fb08a187357c7dc98d350a2cac
ices <2388627880@qq.com>
2019-08-02 11:45:48 +0800

    修复评分标签与分类总数相同的bug

--- src/main/scala/org/example/recommendation/PRTAlgorithm.scala
+++ src/main/scala/org/example/recommendation/PRTAlgorithm.scala
@@ -65,7 +65,8 @@ class PRTAlgorithm(val ap: PRTAlgorithmParams) extends PAlgorithm[PreparedData, 
 
     //3.2 处理处理数据格式
     val trainingData = data.ratings.map(r => {
-      LabeledPoint(r.rating, Vectors.dense(r.user.toInt, r.item.toInt))
+      val t=r.rating
+      LabeledPoint(r.rating-1, Vectors.dense(r.user.toInt, r.item.toInt))
     })
 
     //3.3 准备模型参数
@@ -135,7 +136,7 @@ class PRTAlgorithm(val ap: PRTAlgorithmParams) extends PAlgorithm[PreparedData, 
     val randomModel = model.randomForestModel
     val filtedResult = result.map(r => {
       val v = Vectors.dense(query.user.toInt, r._1.toInt)
-      val rate= randomModel.predict(v)
+      val rate= randomModel.predict(v)+1
       logger.info(s"物品：${r._1}的预测分类为：$rate")
       (r._1,r._2*(rate/5.0))
     })
